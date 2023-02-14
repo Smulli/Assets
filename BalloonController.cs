c349a28ee014ff559a5493f8cbf9a7d4ffa51d10
Smulli <smullimaginario@gmail.com>
2023-02-14 18:11:48 +0100

    BalloonController old added

--- BalloonController.cs
+++ BalloonController.cs
@@ -1,31 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Poplloon.Utilities;
-using Poplloon.Builder;
+using Poplloon.Pooler;
 
-namespace Poplloon.Factory
+namespace Poplloon.Entity
 {
-    public abstract class BalloonController : RecyclableObject
+    public class BalloonController : PoolAble
     {
-        [SerializeField] private int _index;
+        public float _offset;
+        public bool _isMoving;
 
-        public int Index => _index;
-
-        private void Update()
+        public override void Init()
         {
-            if(transform.position.y > 10f)
+            float xPos = Random.Range(-0.1f, 0.1f);
+
+            gameObject.transform.localScale = Vector3.one * Random.Range(2, 7);
+            gameObject.transform.position = new Vector3(xPos, -10f, -7f);
+            gameObject.SetActive(true);
+        }
+
+        private void Start()
+        {
+           _isMoving = true;
+           _offset = UnityEngine.Random.Range(0f, 1f);
+        }
+
+        void Update()
+        {
+            if (_isMoving)
             {
-                Recycle();
+                Vector3 _dir = Vector3.up;
+                float _speed = 1.5f * Time.deltaTime;
+
+                transform.Translate(_dir * _speed + Vector3.right * (Mathf.Sin(Time.time + _offset) / 1200));
             }
-        }
-
-        internal override void Init()
-        {
-        }
-
-        internal override void Release()
-        {
         }
     }
 }
+
