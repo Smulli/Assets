11523b6be650526365e805e63fc5f1a5cb2febe3
Smulli <smullimaginario@gmail.com>
2023-04-30 13:27:42 +0200

    test vue

---
+++ TestVue/mira-softworks/src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="header">
+        <header>
+            <img :src="requiere(`@/assets/${fileLogo}`)" alt="Mira Softworks" widht="850" height="478"/>
+        </header>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Header',
+    props: {
+        fileLogo: String
+    }
+}
+</script>
